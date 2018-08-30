3a38e0dbf817e960c8ae2957bfff9302334986d2
高野翔 <ShoTakano@takanoshou-no-macbook-pro.local>
2018-08-30 16:04:04 +0900

    Revised

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 高野翔 on 2018/08/30.
+//  Copyright © 2018年 高野翔. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
